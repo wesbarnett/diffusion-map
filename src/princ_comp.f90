0f7c12a3b957df780309858989396c4ea909b4a9
James Barnett <jbarnet4@tulane.edu>
2016-12-14 08:55:33 -0600

    cleanup

--- src/princ_comp.f90
+++ src/princ_comp.f90
@@ -9,55 +9,46 @@ module princ_comp
 
 contains
 
-    ! TODO clean up
-    subroutine get_evect_sym(cov,evect,evalues)
+    subroutine get_evect_sym(cov, evect, evalues)
 
         implicit none
-        integer :: N, OK
-        double precision, dimension(:), allocatable ::  work
-        double precision, dimension(:), allocatable, intent(inout) ::  evalues
-        double precision, dimension(:,:), allocatable, intent(inout) :: evect
-        double precision, dimension(:,:), allocatable, intent(inout) :: cov
-        double precision, dimension(:,:), allocatable :: A
-        integer, allocatable, dimension(:) :: ORDER
-        logical, allocatable, dimension(:) :: mask
-        integer :: I
-        integer :: LWORK, LDA
         character :: jobz = 'V', uplo = 'U'
+        real(8), allocatable ::  work(:), a(:,:)
+        real(8), allocatable, intent(inout) ::  evalues(:), evect(:,:), cov(:,:)
+        integer, allocatable :: order(:)
+        integer :: i, lwork, lda, n, info
+        logical, allocatable :: mask(:)
 
         interface 
-          subroutine dsyev(jobz,uplo,N,A,LDA,w,work,LWORK,INFO)
+          subroutine dsyev(jobz, uplo, n, a, lda, w, work, lwork, info)
             character, intent(in) :: jobz, uplo
-            integer, intent(in) :: N, LDA, LWORK
-            integer, intent(out) :: INFO
-            double precision, intent(inout) :: A(LDA,*)
-            double precision, intent(out) :: w(*), work(*)
+            integer, intent(in) :: n, lda, lwork
+            integer, intent(out) :: info
+            real(8), intent(inout) :: a(lda,*)
+            real(8), intent(out) :: w(*), work(*)
           end subroutine
         end interface
 
-        A = cov
-        N = size(A,1)
-        LWORK = 26*N
-        LDA = N
-        allocate(work(LWORK))
-        allocate(evect(N, N))
-        allocate(evalues(N))
+        a = cov
+        n = size(a,1)
+        lwork = 26*n
+        lda = n
+        allocate(work(lwork), evect(n,n), evalues(n))
 
-!       Get eigenvectors and eigenvalues (LAPACK)
-        call dsyev(jobz,uplo,N,A,LDA,evalues,work,LWORK,OK)
+        ! Get eigenvectors and eigenvalues (LAPACK)
+        ! matrix "a" is returned as the eigenvectors
+        call dsyev(jobz, uplo, n, a, lda, evalues, work, lwork, info)
 
-!       Sort the evalues in descending so we can use the top components for
-!       later calcs
-        allocate(ORDER(N))
-        allocate(mask(N))
+        ! Sort the evalues in descending so we can use the top components for later calcs
+        allocate(order(N), mask(n))
         mask = .true.
-        do I = lbound(evalues,1), ubound(evalues,1)
-            ORDER(I) = maxloc(evalues,1,mask)
-            mask(ORDER(I)) = .false.
+        do i = lbound(evalues,1), ubound(evalues,1)
+            order(i) = maxloc(evalues,1,mask)
+            mask(order(i)) = .false.
         end do
 
-        evalues = evalues(ORDER)
-        evect = A(:,ORDER)
+        evalues = evalues(order)
+        evect = a(:,order)
 
     end subroutine get_evect_sym
 
